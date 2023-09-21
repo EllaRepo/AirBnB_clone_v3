e3e4e523d11bad8bcfe55c13e04cc2d8b6330831
thomaskitaba <thomaskitaba@gmail.com>
2023-09-21 21:03:41 +0300

    task 9 correction

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,24 +3,24 @@
 from models import storage
 from models.user import User
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 
 
 @app_views.route('/users', methods=['GET'],
                  strict_slashes=False)
 def users():
     """return all users """
-    res = [
+    my_users = [
         user.to_dict() for user in storage.all(User).values()
     ]
-    return jsonify(res), 200
+    return make_response(jsonify(my_users), 200)
 
 
 @app_views.route('/users/<user_id>', methods=['GET'],
                  strict_slashes=False)
 def user_by_id(user_id):
     """Return users based on user_id"""
-    res = storage.get(User, user_id)
+    res = storage.get("User", user_id)
     if res is None:
         abort(404)
     return jsonify(res.to_dict())
@@ -30,12 +30,12 @@ def user_by_id(user_id):
                  strict_slashes=False)
 def delete_user(user_id):
     """delete user based on provided user_id"""
-    user = storage.get(User, user_id)
+    user = storage.get("User", user_id)
     if user is None:
         abort(404)
     user.delete()
     storage.save()
-    return jsonify({})
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/users', methods=['POST'],
@@ -51,14 +51,14 @@ def insert_user():
         return abort(400, {'message': 'Missing password'})
     new_user = User(**data)
     new_user.save()
-    return jsonify(new_user.to_dict()), 201
+    return make_response(jsonify(new_user.to_dict()), 201)
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_user_by_id(user_id):
     """update city based on given city_id"""
-    user = storage.get(User, user_id)
+    user = storage.get("User", user_id)
     if user is None:
         abort(404)
     data = request.get_json()
@@ -68,4 +68,4 @@ def update_user_by_id(user_id):
         if key not in ["id", "email", "created_at", "updated_at"]:
             setattr(user, key, value)
     storage.save()
-    return jsonify(user.to_dict()), 200
+    return make_response(jsonify(user.to_dict()), 200)
